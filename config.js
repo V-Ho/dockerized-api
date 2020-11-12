32e1896758aa09c82f8ec67d6253bc347114a889
Vanessa Ho <vanessa.ho@externals.adidas.com>
2020-11-12 18:32:24 +0100

    Update configs to use pg-promise

--- config.js
+++ config.js
@@ -1,13 +1,22 @@
 const dotenv = require('dotenv')
-const knex = require('knex')
-
 dotenv.config()
+
+const initOptions = {
+  error(error, e) {
+      if (e.cn) {
+          console.log('CN:', e.cn);
+          console.log('EVENT:', error.message || error);
+      }
+  }
+}
+
+const pgp = require('pg-promise')(initOptions)
 
 const { Pool } = require('pg')
 const isProduction = process.env.NODE_ENV === 'production'
-
 const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@db:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
+const db = pgp(connectionString)
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
   ssl: isProduction
@@ -15,6 +24,8 @@ const pool = new Pool({
 
 module.exports = {
   pool,
+  db,
+  isProduction,
   PORT: process.env.PORT,
   HOST: process.env.HOST,
   DB_USER: process.env.DB_USER,
