794429d6f9ce0bd12bf7efb528581929a30b5cd7
Vanessa Ho <vanessa.ho@externals.adidas.com>
2020-11-12 18:30:35 +0100

    Add routes for user registration and signin

--- server.js
+++ server.js
@@ -1,62 +1,89 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const bcrypt = require('body-parser')
+const bcrypt = require('bcrypt')
 const cors = require('cors')
-const { pool, PORT, HOST } = require('./config')
+const { db, PORT, HOST } = require('./config')
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
-  pool.query('SELECT * FROM users')
+  db.any('select * from users')
   .then(results => {
-   res.status(200).json(results.rows)
-  })
-  .catch(err => res.status(400).json('No users found'))
+    res.status(200)
+    .json({
+      status: 'success',
+      data: results,
+      message: 'Retrieved all users'
+    })
+  }).catch(err => res.status(400).json('No users found'))
+})
+
+app.post('/signin', (req, res) => {
+  const { email, password } = req.body
+
+   db.query('select email, hash from login where login.email = $1', email)
+    .then(results => {
+      console.log('results', results)
+      if (results.length) {
+        let user = results[0]
+        const isValid = bcrypt.compareSync(password, user.hash)
+        console.log('isValid', isValid)
+        if (isValid) {
+          return db.query('select * from users where users.email = $1', email)
+            .then(result => {
+              let user = result[0]
+             return res.status(200).json(user)
+            })
+            .catch(err => res.status(400).json('wrong credentials'))
+        }
+      }
+      return res.status(400).json('wrong credentials')
+    }).catch(err => res.status(400).json('unable to get user'))
 })
 
 app.post('/register', (req, res) => {
-  const { email, name } = req.body
-  pool.query(
-    'INSERT INTO users (email, name, joined) VALUES ($1, $2, now())',
-    [email, name])
-    .then(user => {
-      res.status(201).json({ status: 'success', message: 'user added'})
-    })
-    .catch(err => res.status(400).json('unable to register'))
-})
+  const { email, name, password } = req.body
 
-const checklogin = (email, password) => {
-  const verifiedUser = database.users.filter(user => user.email === email && user.password === password)
-  return verifiedUser.length > 0 ? 'success' : 'error'
-}
+  const salt = bcrypt.genSaltSync()
+  const hash = bcrypt.hashSync(password, salt)
+
+  return db.tx(t => { 
+    return t.any('SELECT * from users where users.email = $1', [email])
+      .then(results => {
+        if (results.length) {
+          throw new Error('user already exists')
+          //return res.status(404).json('user already exists')
+        } else {
+          return t.batch([
+            t.any('INSERT INTO users (email, name, joined) VALUES ($1, $2, now())', [email, name]),
+            t.any('INSERT INTO login (hash, email) VALUES ($1, $2)', [hash, email])
+          ])
+        }
+      })
+  }).then(results => {
+    return res.status(201).json({ status: 'success', message: 'user added'})
+  }).catch(err => {
+    console.log(err)
+    return res.status(400).json('user not registered')
+  })
+})
 
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params
-  pool.query(
-    'SELECT * FROM users WHERE users.id = $1',
-    [id])
+  return db.any('SELECT * FROM users WHERE users.id = $1', [id])
     .then(results => {
-      if (results.rows.length) {
-        res.json(results.rows[0])
+      console.log('id res', results)
+      if (results.length) {
+        return res.json(results[0])
       } else {
-        res.status(400).json('user not found')
+        return res.status(400).json('user not found')
       }
     })
     .catch(err => res.status(400).json('error getting user'))
-})
-
-app.post('/signin', (req, res) => {
-  bcrypt.compare('')
-  if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-    res.json('success')
-  } else {
-    res.status(400).json('error logging in')
-  }
-  res.json('signing in')
-})
+  })
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`app is running on http://${HOST}:${PORT}`)
