fa0497147e37f7fbadafd595ece99ea8e89ab40d
Vanessa Ho <vanessa.ho@externals.adidas.com>
2020-11-15 18:23:56 +0100

    Update configs for pg-promise

--- config.js
+++ config.js
@@ -12,18 +12,17 @@ const initOptions = {
 
 const pgp = require('pg-promise')(initOptions)
 
-const { Pool } = require('pg')
+//const { Pool } = require('pg')
 const isProduction = process.env.NODE_ENV === 'production'
 const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@db:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const db = pgp(connectionString)
-const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction
-})
+// const pool = new Pool({
+//   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+//   ssl: isProduction
+// })
 
 module.exports = {
-  pool,
   db,
   isProduction,
   PORT: process.env.PORT,
